645e667da1289a42790d1b3c12128f2e2fd357b2
guorunhe <Grh121#microsoft>
2021-03-21 02:08:32 -0700

    clear

---
+++ src/sql/clearDatabase.sql
@@ -0,0 +1,17 @@
+-- drop all tables
+drop table CCOMMENT;
+drop table WATCH;
+drop table RREVIEW;
+drop table HISTORYLIST;
+drop table DISCUSSIONCONTENT;
+drop table DISCUSSIONGROUP;
+drop table FAVOURITELIST;
+drop table MMCONTAIN;
+drop table MOVIELIST;
+drop table RMCONTAIN;
+drop table MOVIEBASICINFO;
+drop table MOVIECOMPANYINFO;
+drop table MOVIECOUNTRYINFO;
+drop table MOVIECOMPANY;
+drop table RANKING;
+drop table USERS;
