e97fc7178c895642895c69850bf4f0bb914d0b19
ryang07 <ryang07@students.cs.ubc.ca>
2021-03-22 16:26:55 +0800

    Update quotes

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -1,33 +1,33 @@
 
 -- Deletion: Delete a comment of oneself from CComment
 DELETE FROM CComment
-WHERE CommentID = "$CommentID" AND AccountNumber = "$AccountNumber";
+WHERE CommentID = '$CommentID' AND AccountNumber = '$AccountNumber';
 
 -- Deletion: Delete a review of oneself from RReview
 DELETE FROM CComment
-WHERE CommentID = "$CommentID" AND AccountNumber = "$AccountNumber";
+WHERE CommentID = '$CommentID' AND AccountNumber = '$AccountNumber';
 
 -- Update: Update a user’s password
 UPDATE Users
-SET Passwords = "$newPasswords"
-WHERE AccountNumber = "$AccountNumber";
+SET Passwords = '$newPasswords'
+WHERE AccountNumber = '$AccountNumber';
 
 -- Selection/Projection: Select the title of movies that are produced in a particular year
 SELECT title
 FROM MovieBasicInfo
-WHERE Years = "$Years";
+WHERE Years = '$Years';
 
 -- Selection/Projection: Select the title of movies that are produced by a specific movie company
 SELECT mo.title
 FROM MovieCompanyInfo mo, MovieCompany co
-WHERE mo.CompanyID = co.CompanyID AND co.names = "$Names";
+WHERE mo.CompanyID = co.CompanyID AND co.names = '$Names';
 
 
 -- Join: Users want to check if a specific movie is in their favourite lists
 SELECT (*)
 FROM MovieList ml, MMContain mc, FavouriteList fl
 WHERE ml.ListID = mc.ListID AND ml.ListID = fl.ListID
-      AND mc.MovieID = "$MovieID" AND ml.AccountNumber = "$AccountNumber";
+      AND mc.MovieID = '$MovieID' AND ml.AccountNumber = '$AccountNumber';
 
 
 -- Division: Find all user’s favourite lists that have all movies rating larger than 9.5
