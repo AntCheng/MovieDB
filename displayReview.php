f80d615b63bba6c165a998b08bc199223bcd85a3
guorunhe <Grh121#microsoft>
2021-03-28 15:08:50 -0700

    Add

--- displayReview.php
+++ displayReview.php
@@ -30,11 +30,12 @@ echo "Check out previous reviews of "."<i>".$movieName."</i>"."<br>";
 //get reviews
 $result = executePlainSQL("SELECT u.Names, r.Dates, r.Content, r.ReviewID FROM RReview r, Users u
                                   WHERE (MovieID = '$mid' AND r.AccountNumber = u.AccountNumber)");
-
-echo "<br>  User"." &nbsp; &nbsp; "."  Date ". "&nbsp; &nbsp;".  " Review"." &nbsp;&nbsp;&nbsp; "." ID<br>";
-while (($row = oci_fetch_row($result)) != false) {
-    echo "<br>  " . $row[0] . " &nbsp; " . $row[1]. " &nbsp;  " . $row[2]. " &nbsp;  " . $row[3]. "  <br>";
-}
+$c = 4;
+showTable($result, $c);
+// echo "<br>  User"." &nbsp; &nbsp; "."  Date ". "&nbsp; &nbsp;".  " Review"." &nbsp;&nbsp;&nbsp; "." ID<br>";
+// while (($row = oci_fetch_row($result)) != false) {
+//     echo "<br>  " . $row[0] . " &nbsp; " . $row[1]. " &nbsp;  " . $row[2]. " &nbsp;  " . $row[3]. "  <br>";
+// }
 
 // jump to addReview
 $url = "addReview.php?mid=".urlencode($mid)."&uid=".urlencode($uid);
