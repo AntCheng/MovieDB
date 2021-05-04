d1561cf7b89b39b1fe6e92926f5ac1f9fc58495a
ryang07 <ryang07@students.cs.ubc.ca>
2021-05-04 01:12:40 -0700

    test commit

--- addReview.php
+++ addReview.php
@@ -31,28 +31,6 @@ $url = "main.php?uid=".urlencode($uid);
     <div class="form-group">
         <label><h4>Rating: from 1 to 10</h4></label>
         <form method="POST">
-            <!-- <div class="form_input">
-                <input type="radio" name="rating" value="1">
-                <label for="1"> 1</label><br>
-                <input type="radio" name="rating" value="2">
-                <label for="2"> 2</label><br>
-                <input type="radio" name="rating" value="3">
-                <label for="3"> 3</label><br>
-                <input type="radio" name="rating" value="4">
-                <label for="4"> 4</label><br>
-                <input type="radio" name="rating" value="5">
-                <label for="5"> 5</label><br>
-                <input type="radio" name="rating" value="6">
-                <label for="6"> 6</label><br>
-                <input type="radio" name="rating" value="7">
-                <label for="7"> 7</label><br>
-                <input type="radio" name="rating" value="8">
-                <label for="8"> 8</label><br>
-                <input type="radio" name="rating" value="9">
-                <label for="9"> 9</label><br>
-                <input type="radio" name="rating" value="10">
-                <label for="10"> 10</label><br>
-            </div> -->
 
             <input type="range" min="1" max="10" step="1" value="1" id="rating" name="rating" style="width:500px" onchange='document.getElementById("r").value = document.getElementById("rating").value;'/>
             <input type="text" name="r" id="r" value="1" size="1" style="border:3px solid #FB6107; width:35px; font-size: 20px; background-color: #1BE7FF; text-align: center;" disabled/>
@@ -137,21 +115,14 @@ function handlePOSTRequest() {
 // HANDLE ALL GET ROUTES
 // A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
 function handleGETRequest() {
-    /*    if (connectToDB()) {
-            if(array_key_exists('displayReviews',$_GET)){
-                handleDisplayRequest();
-            }
-            disconnectFromDB();
-        }*/
+
 }
 
 
 if (isset($_POST['submit']) || isset($_POST['return'])) {
     handlePOSTRequest();
 }
-//else if (isset($_GET['displayReviewsRequest'])) {///
-//    handleGETRequest();
-//}
+
 
 
 ?>
